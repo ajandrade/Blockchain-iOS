6372714d3c36575eb6e48e60a9f5461e81203a1f
Amadeu Andrade <amadeu.dev@gmail.com>
2018-01-22 20:46:24 +0000

    Added basic models

--- BlockchainCore.playground/Contents.swift
+++ BlockchainCore.playground/Contents.swift
@@ -1,7 +1,43 @@
 //: Playground - noun: a place where people can play
 
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
-str.sha1()
+struct Transaction {
+  var from: String
+  var to: String
+  var amount: Double
+}
+
+class Block {
+  var index: Int = 0
+  var previousHash: String = ""
+  var hash: String!
+  var nonce: Int = 0
+  
+  private (set) var transactions: [Transaction] = []
+  
+  var key: String {
+    return ""
+  }
+  
+  func addTransaction(_ transaction: Transaction) {
+    transactions.append(transaction)
+  }
+}
+
+class Blockchain {
+  private (set) var blocks: [Block] = []
+  
+  init(genesisBlock: Block) {
+    addBlock(genesisBlock)
+  }
+  
+  func addBlock(_ block: Block) {
+    if blocks.isEmpty {
+      block.previousHash = "0000000000000000"
+    }
+    blocks.append(block)
+  }
+}
+
 
