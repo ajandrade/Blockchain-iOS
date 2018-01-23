d6c147f302595ec66022e36cc8c368a68df9a119
Amadeu Andrade <amadeu.dev@gmail.com>
2018-01-23 18:07:12 +0000

    Minor code refactor

--- BlockchainCore.playground/Contents.swift
+++ BlockchainCore.playground/Contents.swift
@@ -2,11 +2,32 @@
 
 import Foundation
 
+struct HashGenerator {
+  static func generateHash(for block: Block) -> String {
+    var hash = block.key.sha1()
+    // last block @ 22/01/2018:
+    // 0000000000000000000ab398448c2713f3c8dd65a490857ef6e885128f812c85
+    while(!hash.hasPrefix("00")) {  // For simplicity (and performance :)), I'll keep two 0s instead of 19
+      block.nonce += 1
+      hash = block.key.sha1()
+      print(hash)
+    }
+    return hash
+  }
+}
+
 struct Transaction: Codable {
   // PROPERTIES
   var from: String
   var to: String
   var amount: Double
+  
+  // FUNCTIONS
+  func asString() -> String {
+    guard let transactionsData = try? JSONEncoder().encode(self),
+      let transactionsJSONString = String(data: transactionsData, encoding: .utf8) else { return "" }
+    return transactionsJSONString
+  }
 }
 
 class Block {
@@ -19,7 +40,8 @@ class Block {
   private (set) var transactions: [Transaction] = []
   
   var key: String {
-    return String(index) + previousHash + String(nonce) + transactionsAsString()
+    let transactionsInfo = transactions.reduce("") { $0 + $1.asString() }
+    return String(index) + previousHash + String(nonce) + transactionsInfo
   }
   
   // FUNCTIONS
@@ -27,12 +49,6 @@ class Block {
     transactions.append(transaction)
   }
   
-  // HELPER FUNCTIONS
-  private func transactionsAsString() -> String {
-    guard let transactionsData = try? JSONEncoder().encode(transactions),
-      let transactionsJSONString = String(data: transactionsData, encoding: .utf8) else { return "" }
-    return transactionsJSONString
-  }
 }
 
 class Blockchain {
@@ -48,7 +64,7 @@ class Blockchain {
   func addBlock(_ block: Block) {
     if blocks.isEmpty {
       block.previousHash = "0000000000000000"
-      block.hash = generateHash(for: block)
+      block.hash = HashGenerator.generateHash(for: block)
     }
     blocks.append(block)
   }
@@ -56,28 +72,17 @@ class Blockchain {
   func getNextBlock(transactions: [Transaction]) -> Block {
     let block = Block()
     transactions.forEach(block.addTransaction)
-    let previousBlock = getPreviousBlock()
+    guard let previousBlock = getPreviousBlock() else {
+      fatalError("Genesis block is missing!")
+    }
     block.index = blocks.count
     block.previousHash = previousBlock.hash
-    block.hash = generateHash(for: block)
+    block.hash = HashGenerator.generateHash(for: block)
     return block
   }
   
-  func getPreviousBlock() -> Block {
-    return blocks.last ?? Block()
-  }
-  
-  // HELPER FUNCTIONS
-  private func generateHash(for block: Block) -> String {
-    var hash = block.key.sha1()
-    // last block @ 22/01/2018:
-    // 0000000000000000000ab398448c2713f3c8dd65a490857ef6e885128f812c85
-    while(!hash.hasPrefix("00")) {  // For simplicity (and performance :)), I'll keep two 0s instead of 19
-      block.nonce += 1
-      hash = block.key.sha1()
-      print(hash)
-    }
-    return hash
+  func getPreviousBlock() -> Block? {
+    return blocks.last
   }
 }
 
@@ -86,9 +91,13 @@ class Blockchain {
 print("------------------------------\n -- GENESIS BLOCK -- \n------------------------------")
 let genesisBlock = Block()
 let blockchain = Blockchain(genesisBlock: genesisBlock)
-let transaction = Transaction(from: "x1", to: "x2", amount: 20)
+
+let transaction1 = Transaction(from: "x1", to: "x2", amount: 20)
+let transaction2 = Transaction(from: "x3", to: "x4", amount: 14)
+
 print("------------------------------\n -- BLOCK w/ TRANSACTION -- \n------------------------------")
-let block = blockchain.getNextBlock(transactions: [transaction])
+let block = blockchain.getNextBlock(transactions: [transaction1, transaction2])
 blockchain.addBlock(block)
 print("------------------------------\n -- END -- \n------------------------------")
 print("Number of blocks: \(blockchain.blocks.count)")
+
